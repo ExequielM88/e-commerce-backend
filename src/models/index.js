25875963ba03bc7cf38a685ac3e24c98b16613ae
Exequielm88 <kekelom88@gmail.com>
2023-07-01 14:18:53 -0300

    avan

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,27 @@
 const Product = require("./Product")
 const Category = require("./Category")
+const User = require("./User")
+const Cart = require("./Cart")
+const Purchase = require("./Purchase")
+
+
 
 //Product -> //categoryId
 Product.belongsTo(Category)
 Category.hasMany(Product)
+
+//Cart -> //userId
+Cart.belongsTo(User)
+User.hasOne(Cart)
+
+//Cart -> //productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
+
+//Purchase -> //userId
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+//Purchase -> //productId
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
